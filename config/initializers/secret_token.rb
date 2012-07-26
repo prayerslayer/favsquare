07bdbe389b5de3b5f612344c3eb25742e358c43c
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-07-26 21:55:59 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Favsquare::Application.config.secret_token = '9908747f61bb9170c3ddcbac48111fc3147dd32a97efc70757a2e4deb3f119b638ca76a2c22260d743bd2e5d8f621281e9df7e24d1c75fb108d0d2c8fb6395dd'
