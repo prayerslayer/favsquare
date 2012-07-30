70b9a47adfea6f293ce71c8ef6d3724dff762878
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-07-30 20:33:26 +0200

    sequel migration worked

--- db/migrations/001_create_tables.rb
+++ db/migrations/001_create_tables.rb
@@ -1,19 +1,19 @@
 Sequel.migration do
 	change do
 		# create table users
-		:create_table(:users) do
+		create_table(:users) do
 			String :user_id # because we want to store a hash of the user id
 			primary_key :user_id
 		end
 
 		# table tracks
-		:create_table(:tracks) do
+		create_table(:tracks) do
 			primary_key :track_id
 			String :embed_code
 		end
 
 		# table user_tracks
-		:create_table(:user_tracks) do
+		create_table(:user_tracks) do
 			foreign_key(:track_id, :artists)
 			foreign_key(:user_id, :users)
 			primary_key([:user_id, :track_id])
