71e4fc6af2d0646662d79707f00ecd25f6b9b365
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-10-09 08:51:08 +0200

    migrations

--- db/migrations/001_create_tables.rb
+++ db/migrations/001_create_tables.rb
@@ -2,20 +2,25 @@ Sequel.migration do
 	change do
 		# create table users
 		create_table(:users) do
-			String :user_id, :null => false 	# because we want to store a hash of the user id
-			primary_key :user_id
+			String :user_id, :primary_key => true 	# because we want to store a hash of the user id
 		end
 
 		# table tracks
 		create_table(:tracks) do
-			Integer :track_id, :null => false
-			primary_key :track_id
+			Integer :track_id, :primary_key => true
 		end
 
 		# table user_tracks
 		create_table(:user_tracks) do
-			foreign_key :track_id, :tracks, :null => false
-			foreign_key :user_id, :users, :null => false
+			foreign_key :track_id, :tracks, {
+				:null => false,
+				:key => :track_id
+			}
+			foreign_key :user_id, :users, {
+				:null => false,
+				:key => :user_id,
+				:type => String
+			}
 			primary_key( [ :user_id, :track_id ] )
 			Integer :times_served, :default => 0
 		end
--- db/migrations/002_add_faved_by.rb
+++ db/migrations/002_add_faved_by.rb
@@ -2,6 +2,7 @@ Sequel.migration do
 	change do
 		alter_table( :user_tracks ) do
 			add_column :faved_by, Integer, :default => 0
+			add_index :user_id
 		end
 	end
 end
