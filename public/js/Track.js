5fcfb7b130aa0c821d5342c8ddc76731c62af958
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-10-14 19:33:15 +0200

    less configuration among js files

--- public/js/Track.js
+++ public/js/Track.js
@@ -1,11 +1,5 @@
 Track = Backbone.Model.extend({
 	
-	defaults: {
-		id: 0,
-		title: "Unnamed track",
-		permalink: "http://who.car.es/notme"
-	},
-
 	initialize: function() {
 		
 	}
--- public/js/streaming.js
+++ public/js/streaming.js
@@ -2,8 +2,8 @@ $( document ).ready( function() {
 
 	var Favsquare = (function() {
 
-		var //host = "http://localhost:9393",
-			host = "http://obscure-basin-1623.herokuapp.com",
+		var host = "http://localhost:9393",	//development
+			//host = "http://obscure-basin-1623.herokuapp.com",	//production
 			$spinner = $( "#playing-indicator" ),
 			resource = "tracks/10",
 			playlist = new Playlist({
@@ -19,12 +19,19 @@ $( document ).ready( function() {
 		return {
 			//start everythings
 			init: function() {
+				SC.initialize({
+					//client_id: "fcdca5600531b2292ddc9bfe7008cac6"	//production
+					client_id: "f928c3bc1abd9ffc4c6455d13ababa9d" //development
+				});
+				
 				$spinner.ajaxStart( function() {
 					$(this).attr( "src", "img/spinner.gif" );
 				});
 				$spinner.ajaxStop( function() {
 					$(this).attr( "src", "img/logo-small.png" );
 				});
+				//in order to get rid of the default element (?) in the collection
+				playlist.reset();
 				playlist.fetch({
 					add: true,
 					success: function( ) {
@@ -40,9 +47,6 @@ $( document ).ready( function() {
 		};
 	})();
 
-	SC.initialize({
-		client_id: "fcdca5600531b2292ddc9bfe7008cac6"
-	});
 	$( "#arrow" ).click(function() {
 		Favsquare.fetch();
 	});
