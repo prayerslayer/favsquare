77ceb2acea250d8ce4df5a7de07588f2fa371a32
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-10-14 20:04:02 +0200

    commit for production

--- public/js/streaming.js
+++ public/js/streaming.js
@@ -2,8 +2,8 @@ $( document ).ready( function() {
 
 	var Favsquare = (function() {
 
-		var host = "http://localhost:9393",	//development
-			//host = "http://obscure-basin-1623.herokuapp.com",	//production
+		var //host = "http://localhost:9393",	//development
+			host = "http://obscure-basin-1623.herokuapp.com",	//production
 			$spinner = $( "#playing-indicator" ),
 			resource = "tracks/10",
 			playlist = new Playlist({
@@ -20,8 +20,8 @@ $( document ).ready( function() {
 			//start everythings
 			init: function() {
 				SC.initialize({
-					//client_id: "fcdca5600531b2292ddc9bfe7008cac6"	//production
-					client_id: "f928c3bc1abd9ffc4c6455d13ababa9d" //development
+					client_id: "fcdca5600531b2292ddc9bfe7008cac6"	//production
+					//client_id: "f928c3bc1abd9ffc4c6455d13ababa9d" //development
 				});
 				
 				$spinner.ajaxStart( function() {
