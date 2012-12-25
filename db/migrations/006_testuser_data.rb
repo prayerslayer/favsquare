98728d268df7dd72953a85e6c9d141811b103ff4
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-12-25 16:14:38 +0100

    fixed down migration, finally

--- db/migrations/006_testuser_data.rb
+++ db/migrations/006_testuser_data.rb
@@ -60,11 +60,11 @@ Sequel.migration do
 
 	down do
 		# delete user tracks
-		self[:user_tracks].all.delete
+		self[:user_tracks].delete
 
 		# delete tracks
-		self[:tracks].all.delete
-		
+		self[:tracks].delete # fake filter due to unknown syntax. .all.delete doesn't work
+
 		# delete user
 		self[:users].where( :user_id => "2c82b3db619ea8dbf305b10f263bea85747b055558964c205c014976250d9d5d73258c08c4cb5644985bfb9e7edc3601c1423b4aed106120a5edf262ac7234b4").delete
 
