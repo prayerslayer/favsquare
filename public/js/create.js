28a89baa3da8e4ab547e53f231bcad6d57742ccd
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-12-25 20:36:12 +0100

    consistent alert boxes, yo

--- public/js/create.js
+++ public/js/create.js
@@ -1,23 +1,26 @@
 $( document ).ready( function() {
+	var $response = $( "div#response" );
 	$( "button[data-role=submit]" ).click( function() {
 		$( "#spinner" ).fadeIn(200);
 		$( this ).attr("disabled", "true");
-		$( "p#response" ).addClass( "text-info" ).text( "Doin' work...");
+		$response.fadeIn( 200 );
+		$response.addClass( "alert-info" );
+		$response.find("p").text( "Doin' work...")
 		$.post( "create", {}, function( response, bla, blub ) {
 			console.log( response, bla, blub );
 		})
 		.success( function() {
-			var $paragraph = $( "p#response" );
-			$paragraph.addClass( "text-success" );
-			$paragraph.text( "Yay! Everything went alright.");
+			$response.addClass( "alert-success" );
+			$response.find( "strong" ).text( "Yay!" );
+			$response.find( "p" ).text( "Everything went alright." );
 		})
 		.error( function() {
-			var $paragraph = $( "p#response" );
-			$paragraph.addClass( "text-error" );
-			$paragraph.text( "Oh no! Something went wrong... Sorry!");
+			$response.addClass( "alert-error" );
+			$response.find( "strong" ).text( "Damn!" );
+			$response.find( "p" ).text( "Something went wrong... Sorry!");
 		})
 		.complete( function() {
-			$( "p#response").removeClass( "text-info" );
+			$( "div#response").removeClass( "alert-info" );
 			$( "#spinner" ).fadeOut( 200 );
 		});
 	});
